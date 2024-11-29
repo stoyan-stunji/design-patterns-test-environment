c6cb5e92255998953d092a5aa7308615fdd12741
Stoyan Ivanov <115835362+stoyan-stunji@users.noreply.github.com>
2024-11-29 02:04:17 +0200

    Delete DealershipRepository.java

--- DealershipRepository.java
+++
@@ -1,9 +0,0 @@
-public interface DealershipRepository<T>
-{
-    void save(Dealership dealership);
-    Dealership findById(String id);   
-    
-    void makeListing();
-    void deleteListing();
-    Listing findByFilter(Filter<T> filter);
-}
